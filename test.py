80e1356977e792f656a148a73f8a9489ea61a3f7
Gladys Wong <gladys@notmatter.com>
2016-07-14 23:32:37 -0700

    Add files to repo

---
+++ test.py
@@ -0,0 +1,41 @@
+import RPi.GPIO as GPIO
+import gpios
+
+# Use broadcome board pins
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(gpios.HOOK, GPIO.IN)
+GPIO.setup(gpios.IDLE, GPIO.IN)
+GPIO.setup(gpios.NUM, GPIO.IN)
+
+is_done = 0
+
+while True:
+    digits = ""
+    while not GPIO.input(gpios.HOOK):
+        if not is_done:
+           print "Hook lifted!"
+        is_done = 1
+        num = 0
+        is_active_done = 0
+        while GPIO.input(gpios.IDLE):
+            if not is_active_done:
+               print "Active"
+            is_active_done = 1
+               
+            if GPIO.wait_for_edge(gpios.NUM, GPIO.FALLING, timeout=90) is not None:
+                num += 1 # weird with RISING and BOTH?
+                print "pulse: " + str(num)
+
+        if num != 0:
+            numstr = "0" if (num == 10) else str(num)
+            digits += numstr
+
+    if digits != "":
+        print digits
+
+    if is_done:
+        break
+
+GPIO.cleanup()
+
---
+++ test_butt.py
@@ -0,0 +1,65 @@
+import RPi.GPIO as GPIO
+import gpios
+from statemachine import StateMachine
+
+# Use broadcome board pins
+GPIO.setmode(GPIO.BCM)
+
+# Active low signals
+GPIO.setup(gpios.CLEAR, GPIO.IN)     # clear
+GPIO.setup(gpios.BUTT1, GPIO.IN)     # butt1
+GPIO.setup(gpios.BUTT2, GPIO.IN)     # butt2
+GPIO.setup(gpios.BUTT3, GPIO.IN)     # butt3
+GPIO.setup(gpios.BUTT4, GPIO.IN)     # butt4
+GPIO.setup(gpios.BUTT5, GPIO.IN)     # butt5
+
+butt_num = 0
+
+def idle_state(val):
+    print "IDLE: "
+    while 1:
+        if not GPIO.input(gpios.BUTT1):
+            newState = "BUTT"; butt_num = 1; break;
+        elif not GPIO.input(gpios.BUTT2):
+            newState = "BUTT"; butt_num = 2; break;
+        elif not GPIO.input(gpios.BUTT3):
+            newState = "BUTT"; butt_num = 3; break;
+        elif not GPIO.input(gpios.BUTT4):
+            newState = "BUTT"; butt_num = 4; break;
+        elif not GPIO.input(gpios.BUTT5):
+            newState = "BUTT"; butt_num = 5; break
+    print str(butt_num) + " >> "
+    return (newState, val)
+
+def butt_state(val):
+    print "BUTT: "
+    while 1:
+        if not GPIO.input(gpios.BUTT1):
+            butt_num = 1; print str(butt_num);
+        elif not GPIO.input(gpios.BUTT2):
+            butt_num = 2; print str(butt_num);
+        elif not GPIO.input(gpios.BUTT3):
+            butt_num = 3; print str(butt_num);
+        elif not GPIO.input(gpios.BUTT4):
+            butt_num = 4; print str(butt_num);
+        elif not GPIO.input(gpios.BUTT5):
+            butt_num = 5; print str(butt_num);
+        else:
+            butt_num = 0; break
+    print " >> "
+    return (newState, val)
+
+def end_state(val):
+    print "END: "
+    return ("END", 0)
+
+if __name__== "__main__":
+    m = StateMachine()
+    m.add_state("IDLE", idle_state)
+    m.add_state("BUTT", butt_state)
+    m.add_state("END", end_state, end_state=1)
+    m.set_start("IDLE")
+    m.run(1)
+
+GPIO.cleanup()
+
